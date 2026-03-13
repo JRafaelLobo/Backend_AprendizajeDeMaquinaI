6926286e665db59ce015ee725211b9697d8bebcd
Rafa <jose.lobo@unitec.edu>
2026-03-13 19:19:54 +0000

    Eliminando api inutiles y mejorando endpoints

--- backend/chat/urls.py
+++ backend/chat/urls.py
@@ -5,7 +5,7 @@ from chat.views import ChatListCreateView, MessageListCreateView
 
 urlpatterns = [
     path("", ChatListCreateView.as_view(), name="chat-list-create"),
-    path("<int:chat_id>/messages", MessageListCreateView.as_view(), name="message-list-create"),
+    path("message/<int:chat_id>/", MessageListCreateView.as_view(), name="message-list-create"),
     path("message/", views.chat_view, name="chat-message"),
     path("health/", views.health_view, name="chat-health"),
 
